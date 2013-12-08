f464aaeb7504bcc888f0e4781a1d9597e0dc9769
Maksim Karganov <maksim.karganov@gmail.com>
2013-12-08 16:41:42 +0200

    set container-fluid to center add russian locale

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module Octopus
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.yml').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
   end
 end
